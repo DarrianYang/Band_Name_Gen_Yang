1850b7290705103cf6e8adfe3464693bfaa50b8d
Darrian <darrianyang978@gmail.com>
2023-04-30 19:40:28 -0400

    first commit

---
+++ Band Name Generator.py
@@ -0,0 +1,15 @@
+#Band Name Generator by Darrian Yang 4/30/2023
+
+#Step 1: Display welcome message 
+print ('Welcome to the Band Name Generator')
+
+#Step 2: Ask the user for some input
+city = input ('What city are you from? \n')
+
+#Step 3: Ask the user for another input
+sport = input("What is your favorite sport?\n")
+
+#Step 4: Combine the two inputs to print out to terminal
+print ("Your band name could be:", city + " " + sport)
+
+#Step 5: Name should be printed out onto the terminal
